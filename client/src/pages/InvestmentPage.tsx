5a087f658562d5f9d8e09d662f0e7a8a1b5bc30a
Ivan <ikorovyanskiy04@mail.ru>
2026-05-29 18:15:34 +0300

    Investment-Cards-Half

--- client/src/pages/InvestmentPage.tsx
+++ client/src/pages/InvestmentPage.tsx
@@ -1,9 +1,11 @@
 import { InvestmentHero } from "./InvestmentPlan/sections/InvestmentHero/InvestmentHero";
+import { InvestmentCards } from "./InvestmentPlan/sections/InvestmentCards/InvestmentCard";
 
 export const InvestmentPage = () => {
   return (
     <>
         <InvestmentHero />
+        <InvestmentCards />
     </>
   );
 };
